c50e165bd8a9ee0603cb7c432c81994a35395295
MIMIezno <ronnamiez2@gmail.com>
2023-12-18 22:28:15 +0800

    nabua

---
+++ main.py
@@ -0,0 +1,122 @@
+import datetime
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+from MySQLdb.cursors import DictCursor
+
+
+app = Flask(__name__, static_url_path='/static')
+
+# Configure MySQL
+app.secret_key = "Forum-Secret-Key"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mimi' 
+app.config['MYSQL_DB'] = 'forum'
+
+
+mysql = MySQL(app)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.callproc('user_login', (username, password))
+        result = cur.fetchone()
+        cur.close()
+        if result:
+            session["id"] = result[0]
+            return redirect(url_for('category'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html')
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.callproc('create_user', (username, email, password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('login'))
+        
+    return render_template('register.html')
+
+
+@app.route("/category", methods=["GET", "POST"])
+def category():
+    if not session.get("id", 0):
+        return redirect(url_for("login"))
+    if request.method == 'POST':
+        name = request.form['name']
+        tags = request.form['tags']
+        cur = mysql.connection.cursor()
+        cur.callproc('add_category', (name, tags))
+        result = cur.fetchone()
+        cur.nextset()
+        mysql.connection.commit()
+        cur.close()
+        print(result)
+        return redirect(url_for("add_post", id=result))
+    return render_template('category.html')
+
+
+@app.route("/post/<int:id>", methods=["GET", "POST"])
+def post_details(id):
+    if not session.get("id", 0):
+        return redirect(url_for("login"))
+    cur = mysql.connection.cursor(DictCursor)
+    if request.method == "POST":
+        content = request.form["content"]
+        cur.callproc('add_reply', (id, session["id"], content))
+        mysql.connection.commit()
+    cur.callproc('get_post', (id,))
+    result = cur.fetchone()
+    cur.nextset()
+    cur.callproc('get_post_replies', (id,))
+    replies = cur.fetchall()
+    cur.close()
+    return render_template('post_details.html', post=result, replies=replies)
+
+
+@app.route("/", methods=["GET", "POST"])
+def post():
+    if not session.get("id", 0):
+        return redirect(url_for("login"))
+    cur = mysql.connection.cursor(DictCursor)
+    cur.execute("SELECT * FROM post_view")
+    post_list = cur.fetchall()
+    cur.close()
+    return render_template('post.html', post_list=post_list)
+
+
+@app.route("/logout")
+def logout():
+    session["id"] = None
+    return redirect(url_for("login"))
+
+
+@app.route("/post/add", methods=["GET", "POST"])
+def add_post():
+    if not session.get("id", 0):
+        return redirect(url_for("login"))
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        category_id = request.args.get('id')
+        cur = mysql.connection.cursor()
+        cur.callproc('add_post', (session["id"], title, content, category_id))
+        result = cur.fetchone()
+        cur.nextset()
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("post_details", id=result[0]))
+    return render_template('add_post.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,140 @@
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    `password` VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tags TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE posts (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE replies (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    post_id INT UNSIGNED NOT NULL,
+    author_id INT UNSIGNED NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE VIEW user_view AS
+SELECT id, username, email
+FROM users;
+
+CREATE VIEW post_replies_view AS
+SELECT r.id AS id, r.post_id, u.username AS author_username, r.content, r.created_at
+FROM replies r
+JOIN users u ON r.author_id = u.id
+ORDER BY r.created_at DESC;
+
+CREATE VIEW post_view AS
+SELECT p.id AS id, u.username AS author_username, p.title, p.content, p.created_at, c.`name` AS category_name
+FROM posts p
+JOIN users u ON p.author_id = u.id
+JOIN categories c ON p.category_id = c.id
+ORDER BY p.created_at DESC;
+
+DELIMITER //
+
+CREATE PROCEDURE create_user(
+    IN p_username VARCHAR(100),
+    IN p_email VARCHAR(100),
+    IN p_password VARCHAR(200)
+)
+BEGIN
+    INSERT INTO users (username, email, `password`)
+    VALUES (p_username, p_email, p_password);
+END //
+
+CREATE PROCEDURE add_category(
+    IN name_val VARCHAR(100),
+    IN tags_val TEXT
+)
+BEGIN
+    INSERT INTO categories (`name`, tags)
+    VALUES (name_val, tags_val);
+
+    SELECT LAST_INSERT_ID() as new_category_id;
+END //
+
+CREATE PROCEDURE add_post(
+    IN author_id INT,
+    IN title VARCHAR(255),
+    IN content TEXT,
+    IN category_id_val INT
+)
+BEGIN
+    INSERT INTO posts (author_id, title, content, category_id)
+    VALUES (author_id, title, content, category_id_val);
+
+    SELECT LAST_INSERT_ID() as new_post_id;
+END //
+
+CREATE PROCEDURE add_reply(
+    IN post_id INT,
+    IN author_id INT,
+    IN content TEXT
+)
+BEGIN
+    INSERT INTO replies (post_id, author_id, content)
+    VALUES (post_id, author_id, content);
+END //
+
+CREATE PROCEDURE get_post(
+    IN post_id INT
+)
+BEGIN
+    SELECT * 
+    FROM post_view
+    WHERE id = post_id
+    LIMIT 1;
+END //
+
+CREATE PROCEDURE get_post_replies(
+    IN post_id_val INT
+)
+BEGIN
+    SELECT * 
+    FROM post_replies_view
+    WHERE post_id = post_id_val;
+END //
+
+CREATE PROCEDURE user_login(
+    IN p_username VARCHAR(100),
+    IN p_password VARCHAR(200)
+)
+BEGIN
+    SELECT id
+    FROM users
+    WHERE username = p_username AND `password` = p_password
+    LIMIT 1;
+END //
+
+CREATE TRIGGER post_title BEFORE INSERT
+ON posts
+FOR EACH ROW
+BEGIN
+	SET NEW.title = UPPER(NEW.title);
+END //
+
+DELIMITER ;
